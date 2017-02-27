974328d7fbd40639e835f7c03c50d1cf885c49ae
anuar12 <anuar.yeraliyev@gmail.com>
2017-02-27 10:39:58 -0800

    westgrid scripts added

--- run_resnet_xgboost.py
+++ run_resnet_xgboost.py
@@ -1,20 +1,21 @@
 import numpy as np
-import pydicom
+import dicom
 import pandas as pd
 from models.resnet50 import ResNet50
 from keras.preprocessing import image
 from models.imagenet_utils import preprocess_input, decode_predictions
-import xgboost as xgb
+#import xgboost as xgb
 import os
 import cv2
 import keras.backend as K
 from keras.models import Model
+import time
 
 K.set_image_dim_ordering('th')
 
 
 def get_3d_data(path):
-    slices = [pydicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
     slices.sort(key=lambda x: int(x.InstanceNumber))
     return np.stack([s.pixel_array for s in slices])
 
@@ -34,6 +35,8 @@ def get_data_id(path):
             img = sample_image[i + j]
             img = 255.0 / np.amax(img) * img
             img = cv2.equalizeHist(img.astype(np.uint8))
+            print("Shape: ", img.shape)
+            print(img.sum(axis=0), img.sum(axis=1))
             img = img[dx: ds - dx, dx: ds - dx]
             img = cv2.resize(img, (224, 224))
             tmp.append(img)
@@ -51,7 +54,8 @@ def get_data_id(path):
     return batch
 
 if __name__ == '__main__':
-    folder = "/home/andre/kaggle-dsb-2017/data/sample_images/0a0c32c9e08cc2ea76a71649de56be6d"
+    #folder = "/home/andre/kaggle-dsb-2017/data/sample_images/0a0c32c9e08cc2ea76a71649de56be6d"
+    folder = "/Users/Anuar_the_great/desktop/machine_learning/dsb/sample_images/0a0c32c9e08cc2ea76a71649de56be6d"
     batch = get_data_id(folder)
 
     model = ResNet50(weights='imagenet')
@@ -60,10 +64,12 @@ if __name__ == '__main__':
     img = np.ndarray([1,3,224,224],dtype=np.float32)
     img[0] = batch[0]
     x = preprocess_input(img)
-    layer_name = 'avg_pool'
+    layer_name = 'b1'
     intermediate_layer_model = Model(input=model.input,
                                      output=model.get_layer(layer_name).output)
+    t = time.time()
     intermediate_output = intermediate_layer_model.predict(x)
+    print("TIME ELAPSED FOR FORWARD PASS: ", time.time() - t)
     flat_output = intermediate_output.flatten()
     print(flat_output, len(flat_output))
     #print(flat_output)
@@ -73,5 +79,5 @@ if __name__ == '__main__':
     #x = np.expand_dims(x, axis=0)
     #x = preprocess_input(x)
 
-    preds = model.predict(x)
-    print('Predicted:', decode_predictions(preds))
+    # preds = model.predict(x)
+    # print('Predicted:', decode_predictions(preds))
