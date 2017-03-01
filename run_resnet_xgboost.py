bd756b4694382992396eefe43960330e55804158
Andre Lukas Schorlemmer <andre.schorlemmer@gmail.com>
2017-02-28 20:08:31 -0800

    Fixed memory leak in resnet

--- run_resnet_xgboost.py
+++ run_resnet_xgboost.py
@@ -5,6 +5,7 @@ import os
 import cv2
 import keras.backend as K
 from keras.models import Model
+from glob import glob
 
 K.set_image_dim_ordering('th')
 
@@ -19,7 +20,6 @@ def zero_centering(batch):
 def get_data_id(path):
     sample_image = get_3d_data(path)
     sample_image[sample_image == -2000] = 0
-    # f, plots = plt.subplots(4, 5, sharex='col', sharey='row', figsize=(10, 8))
 
     batch = []
     cnt = 0
@@ -38,40 +38,32 @@ def get_data_id(path):
         tmp = np.array(tmp)
         batch.append(np.array(tmp))
 
-        # if cnt < 20:
-        #     plots[cnt // 5, cnt % 5].axis('off')
-        #     plots[cnt // 5, cnt % 5].imshow(np.swapaxes(tmp, 0, 2))
-        # cnt += 1
-
-    # plt.show()
     batch = np.array(batch)
     return batch
 
 
 def calc_features(input_path, output_path):
-    for folder in glob.glob(input_path+'*'): #TODO figure this function out
-        batch = get_data_id(folder)
-        feats = net.predict(batch)
-        print(feats.shape)
-        np.save(folder, feats)
-
-    batch = get_data_id(folder)
-
     model = ResNet50(weights='imagenet')
-
-
-    img = np.ndarray([1,3,224,224],dtype=np.float32)
-    img[0] = batch[0]
-    x = img
     layer_name = 'avg_pool'
-    intermediate_layer_model = Model(input=model.input,
-                                     output=model.get_layer(layer_name).output)
-    intermediate_output = intermediate_layer_model.predict(x)
-    flat_output = intermediate_output.flatten()
+    intermediate_layer_model = Model(input=model.input, output=model.get_layer(layer_name).output)
 
+    for folder in glob(input_path+'*'):
+        output_basename = os.path.basename(os.path.normpath(folder))
+        output_folder = output_path + output_basename
+        if not os.path.exists(output_folder): #TODO don't create new folder for each patient but add id to each filename
+            os.makedirs(output_folder)
+
+        batch = get_data_id(folder)
+
+
+        img = np.ndarray([len(batch),3,224,224],dtype=np.float32)
+        img = batch
+        intermediate_output = intermediate_layer_model.predict(img)
+        #print(output_basename, intermediate_output, intermediate_output.shape)
+        np.save(output_folder + "/features.np", intermediate_output) #TODO add overwrite flag or some other method to enable batch processing of inputs
 
 if __name__ == '__main__':
-    input_folder = "/home/andre/kaggle-dsb-2017/data/sample_images/"
-    output_folder = "/home/andre/kaggle-dsb-2017/data/resnet_features/"
-    calc_features(input_folder, output_folder)
+    input_directory = "/home/andre/kaggle-dsb-2017/data/sample_images/"
+    output_directory = "/home/andre/kaggle-dsb-2017/data/resnet_features/"
+    calc_features(input_directory, output_directory)
 
