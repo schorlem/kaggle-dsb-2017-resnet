fb7b6f60897b24817e34804cd009179b76371924
Andre Lukas Schorlemmer <andre.schorlemmer@gmail.com>
2017-03-02 16:02:26 -0800

    Implemented prediction for resnet/xgboost classifier - Not tested yet

---
+++ predict_xgboost.py
@@ -0,0 +1,27 @@
+def create_submission(input_directory, output_directory, submission_file, model_file)():
+    clf = pickle.load(open(model_file, "rb"))
+
+    df = pd.read_csv(submission_file)
+
+    patients = []
+    for id in df['id'].tolist():
+        patient = np.load(input_folder+'%s_features.npy' % str(id))
+        patient = patient.reshape(patient.shape[0],patient.shape[1])
+        patient = np.mean(patient, axis = 0)
+        patients.append(patient)
+
+    x = np.array(patients)
+
+    pred = clf.predict(x)
+
+    df['cancer'] = pred
+    df.to_csv(output_directory+"submission.csv", index=False)
+    print(df.head())
+
+
+if __name__ == '__main__':
+    input_directory = "/home/andre/kaggle-dsb-2017/data/resnet_features/"
+    output_directory = "/home/andre/kaggle-dsb-2017/data/resnet_features/"
+    submission_file = "/home/andre/kaggle-dsb-2017/data/stage1_sample_submission.csv"
+    model_file = "/home/andre/kaggle-dsb-2017/data/resnet_features/pima.pickle.dat"
+    create_submission(input_directory, output_directory, submission_file, model_file)
